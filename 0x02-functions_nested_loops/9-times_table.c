8e082cfdf21fbe9514175d467051b6d6d5ce438b
kaleb <kalebwendwessen@gmail.com>
2023-12-19 02:07:12 -0800

    Time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * times_table - Print the 9 times table starting from 0
+ */
+void times_table(void)
+{
+	int n, mult, prod;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = n * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
