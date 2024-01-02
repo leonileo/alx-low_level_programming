a3ed836cbe8f86677762d938db14a85b852c4f19
kaleb <kalebwendwessen@gmail.com>
2024-01-02 02:18:30 -0800

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - length of a string
+ * @s: the variable
+ * Return: always 0.
+ */
+int _strlen(char *s)
+{
+int l = strlen(s);
+return (l);
+}
