9300c9b34ee2f177959fc0f3b856ced66f169402
kaleb <kalebwendwessen@gmail.com>
2024-01-16 02:42:06 -0800

    new commit

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if given data a number or not
+ * @c: is an integer param
+ *
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+
+	return (c >= 48 && c <= 57);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+readme for memory allocation malloc function and a function free that
+frees the memory space which have been allocated by a previous call to
+malloc.
