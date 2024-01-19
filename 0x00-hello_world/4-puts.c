ea8acecaf06359f10907854f73ad2148f1e414d1
kaleb <kalebwendwessen@gmail.com>
2024-01-19 09:20:36 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  * main - Entry point
- * Return: Always 0  
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
