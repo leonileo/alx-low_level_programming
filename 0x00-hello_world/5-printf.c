61711868a291763c35dc53eac7ff4ae129b48d1a
kaleb <kalebwendwessen@gmail.com>
2024-01-21 07:31:01 -0800

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h>                                                              
+#include <stdio.h>
 /**
  * main - Entry point
- * Return -1
+ * Return: 1
  */
 
-int main(void)                                                                 
-{                                                                               
-printf("with proper grammar, but the outcome is a piece of art,");            
-return(0);                                                                    
-} 
+int main(void)
+{
+printf("with proper grammar, but the outcome is apiece of art,");
+return (0);
+}
