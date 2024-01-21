2fd41e8853c50eb4a208d284019e42d46578cbe3
kaleb <kalebwendwessen@gmail.com>
2024-01-21 08:39:24 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,18 @@
 
 /**
  * main - Entry point
- * return: (0) Always;
+ *
+ * Return: Always 0 (Success)
  */
-
-char c = sizeof(char);
-int i = sizeof(int);
-long int l = sizeof(long int);
-long long int lli = sizeof(long long int); 
-float f = sizeof(f);
 
 int main(void)
 {
+char c = sizeof(char);
+int i = sizeof(int);
+long int l = sizeof(long int);
+long long int lli = sizeof(long long int);
+float f = sizeof(f);
+
 printf("Size of a char: %d byte(s)\n", c);
 printf("Size of an int: %d byte(s)\n", i);
 printf("Size of a long int: %ld byte(s)\n", l);
