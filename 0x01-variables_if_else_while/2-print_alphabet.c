9170b08a8f994a80bba3097151950b9d6d355c98
kaleb <kalebwendwessen@gmail.com>
2024-01-23 22:05:34 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 success
+*/
+
+int main(void)
+{
+char a;
+char n = '\n';
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+putchar(n);
+
+return (0);
+}
