c2e640797228919dc65f77f53b6567b48c2b06e4
kaleb <kalebwendwessen@gmail.com>
2024-01-30 10:07:29 -0800

    main

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 success
+*/
+
+int main(void)
+{
+const char s = '_';
+const char p = 'p';
+const char u = 'u';
+const char t = 't';
+const char c = 'c';
+const char h = 'h';
+const char a = 'a';
+const char r = 'r';
+const char n = '\n';
+
+_putchar(s);
+_putchar(p);
+_putchar(u);
+_putchar(t);
+_putchar(c);
+_putchar(h);
+_putchar(a);
+_putchar(r);
+_putchar(n);
+
+return (0);
+}
